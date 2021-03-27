fc9f14f7e1101e408bb01e5b2ff85764e61cd456
Shreyash-310 <shreyashgs103@gmail.com>
2021-03-27 15:49:13 +0530

    Create sololearn_func_3.py
    recursion

---
+++ sololearn_func_3.py
@@ -0,0 +1,24 @@
+# rescursion
+"""def factorial(x):
+    if x == 1:
+        return 1
+    else:
+        return x * factorial(x-1)
+print(factorial(5))"""
+
+"""def is_even(x):
+    if x % 2 == 0:
+        return True
+    else:
+        return is_odd(x - 1)
+def is_odd(x):
+    return not is_even(x)
+print(is_odd(18))
+print(is_even(23))"""
+
+"""def fib(x):
+  if x == 0 or x == 1:
+    return 1
+  else:
+    return fib(x-1) + fib(x-2)
+print(fib(4))"""
