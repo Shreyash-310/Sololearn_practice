4058d7e59bb9b2eacc1abf083e24995e3d9d8a2d
Shreyash-310 <shreyashgs103@gmail.com>
2021-03-27 19:09:33 +0530

    Create OOP2.py
    Another OOP file

---
+++ OOP2.py
@@ -0,0 +1,40 @@
+class Vector2D:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        return Vector2D(self.x + other.x, self.y + other.y)
+
+
+first = Vector2D(5, 7)
+second = Vector2D(3, 4)
+result = first + second
+print(result.x)
+print(result.y)
+"""More magic methods for common operators:
+__sub__ for -
+__mul__ for *
+__truediv__ for /
+__floordiv__ for //
+__mod__ for %
+__pow__ for **
+__and__ for &
+__xor__ for ^
+__or__ for |"""
+
+ """ Python also provides magic methods for comparisons.
+__lt__ for <
+__le__ for <=
+__eq__ for ==
+__ne__ for !=
+__gt__ for >
+__ge__ for >="""
+
+ """ There are several magic methods for making classes act like containers.
+ __len__ for len()
+__getitem__ for indexing
+__setitem__ for assigning to indexed values
+__delitem__ for deleting indexed values
+__iter__ for iteration over objects (e.g., in for loops)
+__contains__ for in"""
